ebe2cb5cd9bb951a8fb91a4ee87b99e13ca5c417
alinares <alinares@paradigmadigital.com>
2021-05-01 19:41:45 +0200

    Change script

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
 
 def lambda_handler(event, context):
-    if 'queryStringParameters' in event and 'name' in event['queryStringParameters']:
-        msg = 'Hola  {}!'.format(event["queryStringParameters"]['name'])
+    name = event.get('queryStringParameters', None)
+    if name:
+        msg = 'Hola {}!'.format(name['name'])
     else:
         msg = 'Hola CICE!'
     return{  
         'statusCode': 200,
         'body': json.dumps(msg)
     }
-
